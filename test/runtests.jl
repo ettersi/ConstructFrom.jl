b5a6325477d110cbbcde43c934e52ea73115d66a
Simon Etter <ettersi@users.noreply.github.com>
2023-04-17 14:18:43 +0800

    Add compat entry for julia

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Test
 using ConstructFrom
 
-a::Vector{Int} = construct_from()
-b::Vector{Int} = construct_from(undef, 3)
-c::Dict{Int,Int} = construct_from(1=>0x2, 3.0=>4)
+function foo()
+    a::Vector{Int} = construct_from()
+    b::Vector{Int} = construct_from(undef, 3)
+    c::Dict{Int,Int} = construct_from(1=>0x2, 3.0=>4)
+end
+
+foo()
